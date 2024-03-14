414c2eea8120634e48b51c9c55eb5aedf50188af
tuanddd <vincenttuandao@gmail.com>
2024-03-14 11:24:23 +0700

    fix: web3 packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { LoginWidgetProvider } from "@mochi-web3/login-widget";
+import { Platform } from "@consolelabs/mochi-formatter";
+import { AUTH_TELEGRAM_ID, MOCHI_PROFILE_API } from "@/envs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +15,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <LoginWidgetProvider
+          socials={[
+            Platform.Discord,
+            Platform.Telegram,
+            Platform.Email,
+            Platform.Twitter,
+          ]}
+          telegramBotId={AUTH_TELEGRAM_ID}
+          profileApi={MOCHI_PROFILE_API}
+        >
+          {children as any}
+        </LoginWidgetProvider>
+      </body>
     </html>
   );
 }
