8c8aa7db646cd5161a2de5f84b712bba89aa8ae4
Hien Le <leduyhien152@gmail.com>
2024-05-10 14:47:57 +0700

    fix: staking section

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -27,15 +27,3 @@ export function formatDateToDateTime(date: Date): string {
   const formatter = new Intl.DateTimeFormat("vi-VN", options);
   return formatter.format(date);
 }
-
-export function formatDate(date: Date): string {
-  // Format the date-time string in the desired format
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  };
-
-  const formatter = new Intl.DateTimeFormat("vi-VN", options);
-  return formatter.format(date);
-}
